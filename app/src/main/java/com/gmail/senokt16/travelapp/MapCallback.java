e61f8e7411193fdc2ef61ce70637773fd5b9d9ff
Oktay Sen <senokt.16@gmail.com>
2017-03-11 06:46:17 +0000

    Fixed mappyness

--- app/src/main/java/com/gmail/senokt16/travelapp/MapCallback.java
+++ app/src/main/java/com/gmail/senokt16/travelapp/MapCallback.java
@@ -1,5 +1,6 @@
 package com.gmail.senokt16.travelapp;
 
+import android.graphics.Color;
 import android.location.Location;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -9,6 +10,8 @@ import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Random;
 
 public class MapCallback implements OnMapReadyCallback {
     private GoogleMap mMap;
@@ -28,16 +31,31 @@ public class MapCallback implements OnMapReadyCallback {
 
         mMap = googleMap;
 
+        if (currentPlace == null) {
+            currentPlace = new LatLng(-34, 151);
+        }
+
         // Add a marker in Sydney and move the camera
         CircleOptions putDot = new CircleOptions();
         putDot.center(this.currentPlace);
-        putDot.fillColor(0);
-        putDot.radius(5);
+        putDot.fillColor(Color.BLACK);
+        putDot.radius(500);
         mMap.addCircle(putDot);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(this.currentPlace));
     }
 
     public void onLocationChanged(Location location) {
         this.currentPlace = new LatLng(location.getLatitude(), location.getLongitude());
+        if (mMap != null) {
+            Random rnd = new Random();
+            int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+
+            CircleOptions putDot = new CircleOptions();
+            putDot.center(this.currentPlace);
+            putDot.fillColor(color);
+            putDot.radius(200);
+            mMap.addCircle(putDot);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(this.currentPlace));
+        }
     }
 }
